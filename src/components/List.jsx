0d822f73d2b10dc8df2c8aa0f2be68267bdfcd3b
ArantzaGG <arantzagomez@hotmail.es>
2023-10-20 20:54:38 +0200

    added msj error

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,6 +1,9 @@
 import Card from './Card';
 import '../styles/Card.scss'
 const cardList = ({ filteredScenes }) => {
+    if (filteredScenes.length === 0){
+        return <p className='msjError'>No hay coincidencias, ¡prueba con otro título!</p>
+    }
   const renderCards = filteredScenes.map((scene) => {
     
     return (
